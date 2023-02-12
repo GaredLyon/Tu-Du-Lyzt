38711630071bad84945f10fc8b337a34a9a53972
jaen <jaenfigueroa03042000@gmail.com>
2023-02-12 02:37:13 -0500

    creando carpeta main

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css'
+import { Card } from './components/main/Card'
 
 function App() {
 
   return (
     <div className="App">
-        hola mundo
+      <Card/>
     </div>
   )
 }
