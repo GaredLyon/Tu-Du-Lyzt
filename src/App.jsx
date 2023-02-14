d8b4702cf9bf0b008d26670ff830aee66153c92d
GaredLyon <eddlogg@gmail.com>
2023-02-14 01:24:34 -0600

    create header, modify main content

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Main } from "./components/Main2/Main"
 import './App.css'
 import { Aside } from "./components/Aside/Aside"
+import Header  from "./components/Header/Header"
 
 function App() {
 
@@ -14,7 +15,10 @@ function App() {
       <section className="App__container">
 
         {/* HEADER */}
-        <div className="App__header"></div>
+        <div className="App__header">
+          <Header/>
+          
+        </div>
 
         {/* MAIN */}
         <div className="App__main">
