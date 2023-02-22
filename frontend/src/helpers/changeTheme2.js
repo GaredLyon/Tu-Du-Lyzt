16538a0dbff2258fb9c63a447cbd0e54a39967bf
jaen <jaenfigueroa03042000@gmail.com>
2023-02-21 23:50:16 -0500

    add the ability to switch between multiple themes

---
+++ frontend/src/helpers/changeTheme2.js
@@ -0,0 +1,11 @@
+const cantidadTemas = 3
+let tema = 1
+
+export const changeTheme2 = () => {
+
+  tema++
+
+  if(tema > cantidadTemas) tema = 1
+
+  document.getElementById('home').className = `App tema${tema}`
+}
